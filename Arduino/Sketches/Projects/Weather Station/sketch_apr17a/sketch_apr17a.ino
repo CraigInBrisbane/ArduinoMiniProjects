2bba5df1f05f82a8334ea3410593b72c7ba19639
Craig Lister <craig@listerhome.com>
2019-04-19 01:16:14 +1000

    Added image for Motion Detector and added code for min/max temps for weather station project.

--- Arduino/Sketches/Projects/Weather Station/sketch_apr17a/sketch_apr17a.ino
+++ Arduino/Sketches/Projects/Weather Station/sketch_apr17a/sketch_apr17a.ino
@@ -12,36 +12,114 @@ int OLED_RESET = 4; // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+int ThermistorPin = 0;
+int Vo;
+float R1 = 100000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
-void setup() {
-  // put your setup code here, to run once:
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+float readTemperature() {
 
+  Vo = analogRead(ThermistorPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  T = T - 273.15;
+
+  return T;
+}
+
+void displayDetails(float temp, float minTemp, float maxTemp) {
+  
   display.clearDisplay();
-  display.setFont(&FreeSans9pt7b);
-  display.setTextSize(1);
+  display.setFont();
+  display.setTextSize(2);
   display.setTextColor(WHITE);
 
-  String temp = "T 28.3 ";
-  temp += (char)223;
-  temp += "c";
+  String tempString = "T ";
+  tempString+= temp;
+  tempString+= " 'C";
+  String humidity = "H 69.3 %";
+  String pressure = "P 1080 hPa";
 
- String humidity = "H 69.3% ";
+  display.setCursor(0,0);
+  display.println(tempString);
 
-  display.setCursor(0,12);
-  display.println(temp);
+  display.setTextSize(1);
+  display.setCursor(0, 17);
+
+  String maxs = "Max: ";
+  maxs += maxTemp;
+  maxs += " 'C";
+
+  display.println(maxs);
+
+  display.setCursor(0, 25);
+
+  String mins = "Min: ";
+  mins += minTemp;
+  mins += " 'C";
   
-  display.setCursor(0,31);
+  display.println(mins);
+   
+  
+  /*
+  display.setCursor(0,12);
   display.println(humidity);
-    
+
+  display.setCursor(0,24);
+  display.println(pressure);
+
+  display.setCursor(72,0);
+  display.println("H 28.3 'C");
+  
+  display.setCursor(72,12);
+  display.println("L 12.4 'C");
+      
+  display.setCursor(72,24);
+  display.println("Rising");
+*/
   display.display();
 
 }
 
+void setup() {
+  // put your setup code here, to run once:
+ 
+  pinMode(ThermistorPin, OUTPUT);
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setFont();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("Initialising...");
+  display.display();
+}
+
+unsigned long pauseTime = 3000;
+unsigned long lastMillis = 0;
+float maxTemp = -500;
+float minTemp = 500;
+
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+  if(currentMillis - lastMillis > pauseTime) {
+    float currentTemp = readTemperature();
+    if(currentTemp > maxTemp) {
+      maxTemp = currentTemp;
+    }
 
+    if(currentTemp < minTemp) {
+      minTemp = currentTemp;
+    }
+    displayDetails(currentTemp, minTemp, maxTemp);
+    lastMillis = millis();
+    
+  }
 }
